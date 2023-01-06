9f878496315a1383bf4e8032b5eb8ada0b71f5fe
Nicholas Ikiroma <nicholasikiroma@gmail.com>
2023-01-06 22:25:54 +0100

    storage abstraction

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,20 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy import String, Integer, DateTime, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(),
+                        nullable=False)
+    updated_at = Column(Datetime, default=datetime.utcnow(),
+                        nullable=False)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,12 +23,19 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs.keys():
+                kwargs['updated_at'] =
+                datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'created_at' in kwargs.keys():
+                kwargs['created_at'] =
+                datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if "id" not in kwargs.keys():
+                setattr(self, "id", str(uuid.uuid4()))
+            if "created_at" not in kwargs.keys():
+                setattr(self, "created_at", datetime.now())
+            if "updated_at" not in kwargs.keys():
+                setattr(self, "updated_at", datetime.now())
             del kwargs['__class__']
             self.__dict__.update(kwargs)
 
@@ -41,4 +58,13 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        if '_sa_instance_state' in dictionary.keys():
+            dictionary.pop('_sa_instance_state')
+
         return dictionary
+
+    def delete(self):
+        """Deletes current instance from storage"""
+        from models import storage
+        storage.delete(self)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from os import getenv
+from sqlalchemy.orm import relationship
+import models
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
+
+    else:
+        @property
+        def cities(self):
+            """returns the list of City instances with state_id"""
+            city_instance = []
+            city_dict = models.storage.all(models.city.City)
+            for k, v in city_dict.items():
+                if v.state_id == self.id:
+                    city_instances.append(v)
+            return city_instance
